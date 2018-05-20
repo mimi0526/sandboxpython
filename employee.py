7ad6c5fa1c68c76c30a668f19e300247b2554d7d
Michelle Jiang <michellej@Michelles-MacBook-Pro.local>
2018-05-20 11:39:09 -0700

    add employee class file

---
+++ employee.py
@@ -0,0 +1,13 @@
+class Employee:
+        
+    first_name = "n/a"
+    last_name = "n/a"
+    uid = "n/a"
+        
+def main():
+    e = Employee()
+    print(e.first_name)
+    print(e.last_name)
+    print(e.uid)
+
+main()
